04e9a6494337f4f86fe776a681d8bc8e448daaf6
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-08-31 07:47:08 +0100

    Add tests block

--- token_access_test.rb
+++ token_access_test.rb
@@ -1,4 +1,9 @@
 require 'test_helper'
 
 class TokenAccessTest < ActionDispatch::IntegrationTest
+  test 'allows access for valid tokens' do
+  end
+
+  test 'returns error for invalid or missing tokens' do
+  end
 end
