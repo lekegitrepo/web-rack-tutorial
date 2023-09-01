6308709cb326652755bc1a6119ca780df667949d
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-09-01 07:50:59 +0100

    Add test assertion

--- token_access_test.rb
+++ token_access_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class TokenAccessTest < ActionDispatch::IntegrationTest
   test 'allows access for valid tokens' do
+    get middleware_path, headers: { 'token' => 'my-access-token'}
+    assert_equal 'success', response.body
   end
 
   test 'returns error for invalid or missing tokens' do
